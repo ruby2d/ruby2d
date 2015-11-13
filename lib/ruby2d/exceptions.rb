c578541d393dfa1536d564fd71f85f9a62c824c4
Tom Black <blacktm@gmail.com>
2015-11-13 00:38:46 -0500

    Cleaner exceptions

--- lib/ruby2d/exceptions.rb
+++ lib/ruby2d/exceptions.rb
@@ -2,11 +2,15 @@
 
 module Ruby2D
   class Error < StandardError
+    def colorize(msg, c); "\e[#{c}m#{msg}\e[0m" end
+    def error(msg); colorize(msg, '4;31') end
+    def bold(msg); colorize(msg, '1') end
+    
     def initialize(msg)
       super(msg)
-      puts msg
-      puts "Occurred in:"
-      puts "  " + caller.last, "\n"
+      puts error("\nRuby 2D Error:") << " " << msg
+      puts bold("Occurred in:")
+      puts bold("  " + caller.last), "\n"
     end
   end
 end
