7f749675095383ca816b8469ae53d65c73d708d7
Tom Black <blacktm@gmail.com>
2015-12-29 01:15:43 -0600

    Update Simple 2D using Homebrew when installed
    Also more extconf.rb cleanup

--- ext/ruby2d/extconf.rb
+++ ext/ruby2d/extconf.rb
@@ -16,17 +16,16 @@ def print_errors(errors)
   
   #{errors.join("\n  ")}
   
-#{"============================================================".bold}
-
-"
+#{"============================================================".bold}"
 end
 
 
 # OS X
 if RUBY_PLATFORM =~ /darwin/
-  # If Simple 2D not installed
+  
+  # Simple 2D not installed
   if `which simple2d`.empty?
-    # If Homebrew not installed, print and quit
+    # Homebrew not installed, print and quit
     if `which brew`.empty?
       errors << "Ruby 2D uses a library called Simple 2D." <<
                 "On OS X, this can be installed using Homebrew." <<
@@ -39,14 +38,21 @@ if RUBY_PLATFORM =~ /darwin/
       `brew tap simple2d/tap`
       `brew install simple2d`
     end
-  # Simple 2D is installed, update to latest version
+    
+  # Simple 2D installed, update to latest version
   else
+    # Homebrew not installed
     if `which brew`.empty?
-      # TODO: Check for latest version manually
+      # TODO: Check for latest version manually and update
+    # Homebrew installed, get latest version of Simple 2D
     else
-      # TODO: Get latest version of Simple 2D
-      # `brew update`
-      # `brew upgrade simple2d`
+      # An alternative, but slower and updates all formulas:
+      #   `brew update`
+      #   `brew upgrade simple2d`
+      
+      `brew untap simple2d/tap`
+      `brew tap simple2d/tap`
+      `brew upgrade simple2d`
     end
   end
   
@@ -67,6 +73,7 @@ elsif RUBY_PLATFORM =~ /mingw/
   puts "Ruby 2D doesn't support Windows yet :("
   exit
 end
+
 
 $LDFLAGS << ' ' << `simple2d --libs`
 $LDFLAGS.gsub!(/\n/, ' ')  # remove newlines in flags, they cause problems
