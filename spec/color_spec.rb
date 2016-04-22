417d76099d4979d6d98c796d5425776afdb73ef4
Tom Black <blacktm@gmail.com>
2016-04-21 23:03:35 -0400

    Change tests to use `eq`
    Since we’re just validating output, and not checking if it’s literally
    the same object

--- spec/color_spec.rb
+++ spec/color_spec.rb
@@ -6,21 +6,21 @@ RSpec.describe Ruby2D::Color do
   describe '#is_valid?' do
     
     it 'determines if a color string is valid' do
-      expect(Ruby2D::Color.is_valid? 'red').to be true
-      expect(Ruby2D::Color.is_valid? 'balloons').to be false
+      expect(Ruby2D::Color.is_valid? 'red').to eq true
+      expect(Ruby2D::Color.is_valid? 'balloons').to eq false
     end
     
     it 'determines if an array is a valid color' do
-      expect(Ruby2D::Color.is_valid? [1.0, 0, 0, 1.0]).to be true
-      expect(Ruby2D::Color.is_valid? [1.0, 0, 0]).to be false
+      expect(Ruby2D::Color.is_valid? [1.0, 0, 0, 1.0]).to eq true
+      expect(Ruby2D::Color.is_valid? [1.0, 0, 0]).to eq false
     end
     
     it 'prevents allow color values out of range' do
-      expect(Ruby2D::Color.is_valid? [1.0, 0, 0.0, 255]).to be true
-      expect(Ruby2D::Color.is_valid? [1.2, 0, 0, 0]).to be false
-      expect(Ruby2D::Color.is_valid? [-0.1, 0, 0, 0]).to be false
-      expect(Ruby2D::Color.is_valid? [255, 255, 256, 255]).to be false
-      expect(Ruby2D::Color.is_valid? [-1, 0, 127, 255]).to be false
+      expect(Ruby2D::Color.is_valid? [1.0, 0, 0.0, 255]).to eq true
+      expect(Ruby2D::Color.is_valid? [1.2, 0, 0, 0]).to eq false
+      expect(Ruby2D::Color.is_valid? [-0.1, 0, 0, 0]).to eq false
+      expect(Ruby2D::Color.is_valid? [255, 255, 256, 255]).to eq false
+      expect(Ruby2D::Color.is_valid? [-1, 0, 127, 255]).to eq false
     end
     
   end
