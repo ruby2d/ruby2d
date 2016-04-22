a3df22766dc5139e27024c96271aafb51ef1f316
Tom Black <blacktm@gmail.com>
2016-04-21 23:05:30 -0400

    Silence lib output on RSpec tests
    Namely for testing when exceptions are thrown

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,18 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+  
+  # Silence output when running tests
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    # Restore
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 end
