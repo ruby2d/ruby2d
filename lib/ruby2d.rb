b163a88fc11b654136c60ca3c7f26710d0b73e28
Tom Black <blacktm@gmail.com>
2016-07-18 00:57:50 -0500

    Reorder require statements

--- lib/ruby2d.rb
+++ lib/ruby2d.rb
@@ -1,15 +1,15 @@
 # ruby2d.rb
 
+require 'ruby2d/color'
 require 'ruby2d/window'
 require 'ruby2d/application'
-require 'ruby2d/color'
+require 'ruby2d/dsl'
 require 'ruby2d/quad'
 require 'ruby2d/rectangle'
 require 'ruby2d/square'
 require 'ruby2d/triangle'
 require 'ruby2d/image'
 require 'ruby2d/text'
-require 'ruby2d/dsl'
 require 'ruby2d/exceptions'
 require 'ruby2d/ruby2d'  # load native extension
 
