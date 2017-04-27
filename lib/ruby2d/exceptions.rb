8d39db97fd82e99015d31fa38436fea6c1530a87
Tom Black <blacktm@gmail.com>
2017-04-26 22:16:19 -0400

    Add `File.exists?` to MRuby
    This also enables file checking to the `Image` class for MRI and MRuby
    (on the web, a “failed to load resource” already appears). Also, all
    exceptions will now be printed in MRuby, yay!

--- lib/ruby2d/exceptions.rb
+++ lib/ruby2d/exceptions.rb
@@ -2,14 +2,5 @@
 
 module Ruby2D
   class Error < StandardError
-    def colorize(msg, c); "\e[#{c}m#{msg}\e[0m" end
-    def error(msg); colorize(msg, '4;31') end
-    def bold(msg); colorize(msg, '1') end
-    
-    def initialize(msg)
-      super(msg)
-      puts error("\nRuby 2D Error:") << " #{msg}" <<
-      bold("\nOccurred in:\n  #{caller.last}\n")
-    end
   end
 end
