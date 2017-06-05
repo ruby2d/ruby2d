e87ee92576e6e9a1aec39c77d38a2e14f033c31d
Tom Black <blacktm@gmail.com>
2017-06-04 21:10:33 -0400

    Add all controller events to test

--- test/controller.rb
+++ test/controller.rb
@@ -6,4 +6,16 @@ on :controller do |event|
   puts event
 end
 
+on :controller_axis do |event|
+  puts "Axis: #{event.axis}, Value: #{event.value}"
+end
+
+on :controller_button_down do |event|
+  puts "Button down: #{event.button}"
+end
+
+on :controller_button_up do |event|
+  puts "Button up: #{event.button}"
+end
+
 show
