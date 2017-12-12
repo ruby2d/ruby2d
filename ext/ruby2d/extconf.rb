329122dc753a01d8738c76dcfd1dd5d117452cb2
Tom Black <blacktm@gmail.com>
2017-12-12 15:59:33 -0800

    Bump version to 0.5.0

--- ext/ruby2d/extconf.rb
+++ ext/ruby2d/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
-S2D_VERSION = '0.7.0'  # Simple 2D minimum version required
+S2D_VERSION = '0.8.0'  # Simple 2D minimum version required
 $errors = []
 
 class String
--- lib/ruby2d/version.rb
+++ lib/ruby2d/version.rb
@@ -1,5 +1,5 @@
 # version.rb
 
 module Ruby2D
-  VERSION = '0.4.2'
+  VERSION = '0.5.0'
 end
