50b36b6a8aaf9512a4699734c160fcd82c0c2ea6
Andrew Havens <email@andrewhavens.com>
2017-12-29 09:26:26 -0800

    Check for valid file path to prevent segfault when file does not exist.

--- test/sprite_spec.rb
+++ test/sprite_spec.rb
@@ -3,8 +3,12 @@ require 'ruby2d'
 RSpec.describe Ruby2D::Sprite do
 
   describe '#new' do
+    it "raises exception if file doesn't exist" do
+      expect { Sprite.new(0, 0, "bad_sprite_sheet.png") }.to raise_error(Ruby2D::Error)
+    end
+
     it 'creates a new sprite' do
-      Ruby2D::Sprite.new(0, 0, "tests/media/sprite_sheet.png")
+      Sprite.new(0, 0, "test/media/sprite_sheet.png")
     end
   end
 
