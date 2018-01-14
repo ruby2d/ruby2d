da6cc76768cab77533acdd465a0a779e7465f634
Mad Cookie <mc@cookieoverflow.com>
2018-01-14 10:29:28 +0000

    Add a collided_with? method to the Rectangle class
    This is to make it easier to check collision between rectangles
    
    E.g. `@player.kill if @player.collided_with? @enemy`

--- test/rectangle_spec.rb
+++ test/rectangle_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe Ruby2D::Rectangle do
       expect(rectangle.contains?( 50,  25)).to be false
     end
   end
+
+  describe '#collided_with?' do
+    it "returns false if passed rectangle is not inside rectangle" do
+      rectangleA = Rectangle.new(x: 0, y: 0, width: 50, height: 50)
+      rectangleB = Rectangle.new(x: 100, y: 100, width: 50, height: 50)
+
+      expect(rectangleA.collided_with?(rectangleB)).to be false
+    end
+
+    it "returns true if passed rectangle is inside rectangle" do
+        rectangleA = Rectangle.new(x: 0, y: 0, width: 50, height: 50)
+        rectangleB = Rectangle.new(x: 25, y: 25, width: 50, height: 50)
+
+        expect(rectangleA.collided_with?(rectangleB)).to be true
+    end
+  end
 end
