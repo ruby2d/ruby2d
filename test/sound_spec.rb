9c6c062b197b7ce4a1976e2f38289cea3949fc8b
Tom Black <blacktm@gmail.com>
2019-01-10 21:54:08 -0800

    WASAPI can't be opened on AppVeyor (Windows)

--- test/sound_spec.rb
+++ test/sound_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe Ruby2D::Sound do
 
   describe "#new" do
     it "raises exception if audio file doesn't exist" do
-      expect { Sound.new("bad_sound.wav") }.to raise_error(Ruby2D::Error)
+      expect { Sound.new("no_sound_here.wav") }.to raise_error(Ruby2D::Error)
     end
 
-    it "creates sound" do
-      snd = Sound.new('test/media/music.mp3')
-      expect(snd.path).to eq('test/media/music.mp3')
+    unless ENV['APPVEYOR']
+      it "creates sound" do
+        snd = Sound.new('test/media/sound.wav')
+        expect(snd.path).to eq('test/media/sound.wav')
+      end
     end
   end
 
