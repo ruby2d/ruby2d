c5abd89e03f9b18ff4a8293e9c8160923d58b3a5
Tom Black <blacktm@gmail.com>
2019-01-10 21:36:34 -0800

    Adding some comments

--- lib/ruby2d/cli/console.rb
+++ lib/ruby2d/cli/console.rb
@@ -1,3 +1,6 @@
+# Interactive Ruby 2D console
+
+# Save the Ruby file from the command-line arguments
 rb_file = ARGV[1]
 
 # Check if source file provided is good
@@ -14,11 +17,12 @@ require 'open3'
 require 'readline'
 require 'io/wait'
 
-line = 1  # the current line number, to be incremented
+line = 1  # the current line number
 
-# Open a new process for the  Ruby file
+# Open a new process for the Ruby file
 stdin, stdout, stderr, wait_thr = Open3.popen3("ruby -r 'ruby2d/cli/enable_console' #{rb_file}")
 
+# Request input and send commands
 loop do
 
   # Read the next command
@@ -31,7 +35,9 @@ loop do
     exit
   end
 
+  # Try sending commands
   begin
+
     # Skip if command is an empty string
     unless cmd.empty?
 
@@ -43,12 +49,17 @@ loop do
       while stdout.ready? do
         puts stdout.gets
       end
+
     end
 
-    # Advance to next line
+    # Increment the line number
     line += 1
+
+  # Rescue exception if can't send commands to the Ruby 2D window
   rescue Errno::EPIPE
-    puts "Can't connect to 2d application. See http://www.ruby2d.com/learn/console/#common-issues for more info"
+    puts "Can't connect to the window (was it closed?)",
+         "For help, see: ruby2d.com/learn/console"
     exit 1
   end
+
 end
