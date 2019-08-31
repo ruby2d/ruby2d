be31b3a00cc2d3279e8a6f60d2aab85d8419dc4e
Travis Rasor <travis@thathanka.org>
2019-08-31 17:18:17 -0500

    Implementing sound volume code in ruby2d.c per issue https://github.com/ruby2d/ruby2d/issues/115

--- test/audio.rb
+++ test/audio.rb
@@ -5,12 +5,27 @@ set width: 300, height: 200, title: "Ruby 2D — Audio"
 snd = Sound.new('media/sound.wav')
 mus = Music.new('media/music.wav')
 
-volume_bar = Rectangle.new(color: 'green', width: 300, height: 50)
+music_vol_label = Text.new("Music Volume: 100",  x: 65, y: 10, z:2, color: 'white', size: 20)
+music_volume_bar = Rectangle.new(color: 'green', z:1, width: 300, height: 50)
+music_click_bar = Rectangle.new(color: 'black', width: 300, height: 50)
+
+sound_vol_label = Text.new("Sound Volume: 100",  x: 65, y: 110, z:2, color: 'white', size: 20)
+sound_volume_bar = Rectangle.new(color: 'green', y: 100, z:1, width: 300, height: 50)
+sound_click_bar = Rectangle.new(color: 'black', y: 100, width: 300, height: 50)
 
 on :mouse_down do |event|
-  Music.volume = event.x / Window.width.to_f * 100
-  volume_bar.width = Music.volume / 100.0 * Window.width
-  puts "Music volume: #{Music.volume}%"
+  if music_click_bar.contains? event.x, event.y
+    Music.volume = event.x / Window.width.to_f * 100
+    music_volume_bar.width = Music.volume / 100.0 * Window.width
+    music_vol_label.text = "Music Volume: "+"#{Music.volume}".rjust(3)
+    puts "Music volume: #{Music.volume}%"
+  end
+  if sound_click_bar.contains? event.x, event.y
+    snd.volume = (event.x / Window.width.to_f * 100)
+    sound_volume_bar.width = snd.volume / 100.0 * Window.width
+    sound_vol_label.text = "Sound Volume: "+"#{snd.volume}".rjust(3)
+    puts "Sound volume: #{snd.volume}%"
+  end
 end
 
 on :key_down do |event|
@@ -18,11 +33,13 @@ on :key_down do |event|
   when 'p'
     puts "Playing sound"
     snd.play
+    puts "Sound volume: #{snd.volume}"
   when 'm'
     puts "Playing music"
     mus.play
     mus.volume = 100
-    volume_bar.width = Window.width
+    music_volume_bar.width = Window.width
+    music_vol_label.text = "Music Volume: "+"#{Music.volume}".rjust(3)
     puts "Music volume: #{mus.volume}"
   when 'l'
     puts "Looping music"
