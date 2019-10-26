74f153fdd4c0584a03fccbedb1aa5c8eb643ea9c
TheRealSavi <johngibbons167@gmail.com>
2019-10-26 19:52:26 -0400

    removed handler

--- lib/ruby2d/window.rb
+++ lib/ruby2d/window.rb
@@ -432,39 +432,5 @@ module Ruby2D
       end
     end
 
-    #Slider
-    @@window.on :mouse do |event|
-      if event.button == :left && event.type == :down
-        @dragging = true
-      end
-      if event.button == :left && event.type == :up
-        @dragging = false
-      end
-      if @dragging == true
-        Slider.sliders.each do |slider|
-          if slider.shown && slider.enabled
-            if event.y.between?(slider.y-slider.size,slider.y+slider.size)
-              slider.moveKnob(event.x)
-            end
-          end
-        end
-      end
-    end
-
-    #ButtonList
-    @@window.on :mouse_down do |event|
-      if event.button == :left
-        ButtonList.buttonLists.each do |buttonList|
-          buttonList.options.each do |id , button|
-            if button.shown && button.enabled
-              if event.x.between?(button.x-button.size,button.x+button.size) && event.y.between?(button.y-button.size,button.y+button.size)
-                buttonList.toggle(button)
-              end
-            end
-          end
-        end
-      end
-    end
-
   end
 end
