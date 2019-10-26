9b55cc09b640993735c52af7f3330a4f6837b5df
TheRealSavi <johngibbons167@gmail.com>
2019-10-26 19:48:26 -0400

    moved handler into window

--- lib/ruby2d/window.rb
+++ lib/ruby2d/window.rb
@@ -432,39 +432,39 @@ module Ruby2D
       end
     end
 
-  end
-
-  #Slider
-  on :mouse do |event|
-    if event.button == :left && event.type == :down
-      @dragging = true
-    end
-    if event.button == :left && event.type == :up
-      @dragging = false
-    end
-    if @dragging == true
-      Slider.sliders.each do |slider|
-        if slider.shown && slider.enabled
-          if event.y.between?(slider.y-slider.size,slider.y+slider.size)
-            slider.moveKnob(event.x)
-          end
-        end
+    #Slider
+    self.on :mouse do |event|
+      if event.button == :left && event.type == :down
+        @dragging = true
       end
-    end
-  end
-
-  #ButtonList
-  on :mouse_down do |event|
-    if event.button == :left
-      ButtonList.buttonLists.each do |buttonList|
-        buttonList.options.each do |name, button|
-          if button.shown && button.enabled
-            if event.x.between?(button.x-button.size,button.x+button.size) && event.y.between?(button.y-button.size,button.y+button.size)
-              buttonList.toggle(button)
+      if event.button == :left && event.type == :up
+        @dragging = false
+      end
+      if @dragging == true
+        Slider.sliders.each do |slider|
+          if slider.shown && slider.enabled
+            if event.y.between?(slider.y-slider.size,slider.y+slider.size)
+              slider.moveKnob(event.x)
             end
           end
         end
       end
     end
+
+    #ButtonList
+    self.on :mouse_down do |event|
+      if event.button == :left
+        ButtonList.buttonLists.each do |buttonList|
+          buttonList.options.each do |id , button|
+            if button.shown && button.enabled
+              if event.x.between?(button.x-button.size,button.x+button.size) && event.y.between?(button.y-button.size,button.y+button.size)
+                buttonList.toggle(button)
+              end
+            end
+          end
+        end
+      end
+    end
+
   end
 end
