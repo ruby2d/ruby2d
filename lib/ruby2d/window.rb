a966b3b1233c0b31d620dd61eb4617f15b14d5ed
TheRealSavi <johngibbons167@gmail.com>
2019-10-26 20:10:32 -0400

    keepin it good

--- lib/ruby2d/window.rb
+++ lib/ruby2d/window.rb
@@ -302,6 +302,36 @@ module Ruby2D
     # Mouse callback method, called by the native and web extentions
     def mouse_callback(type, button, direction, x, y, delta_x, delta_y)
       # All mouse events
+      #Slider
+        if button == :left && type == :down
+          @dragging = true
+        end
+        if button == :left && type == :up
+          @dragging = false
+        end
+        if @dragging == true
+          Slider.sliders.each do |slider|
+            if slider.shown && slider.enabled
+              if y.between?(slider.y-slider.size,slider.y+slider.size)
+                slider.moveKnob(x)
+              end
+            end
+          end
+        end
+
+      #ButtonList
+        if button == :left
+          ButtonList.buttonLists.each do |buttonList|
+            buttonList.options.each do |id , button|
+              if button.shown && button.enabled
+                if x.between?(button.x-button.size,button.x+button.size) && y.between?(button.y-button.size,button.y+button.size)
+                  buttonList.toggle(button)
+                end
+              end
+            end
+          end
+        end
+
       @events[:mouse].each do |id, e|
         e.call(MouseEvent.new(type, button, direction, x, y, delta_x, delta_y))
 
