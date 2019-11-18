2677f6c37d25e4d2a69016b3b85871385b7d26df
wmoxam <wes@wmoxam.com>
2019-11-17 23:17:45 -0500

    Refactor

--- lib/ruby2d/font.rb
+++ lib/ruby2d/font.rb
@@ -61,35 +61,23 @@ module Ruby2D
 
       # Get the fonts directory for the current platform
       def directory
-        macos_font_path   = '/Library/Fonts'
-        linux_font_path   = '/usr/share/fonts'
-        windows_font_path = 'C:/Windows/Fonts'
-        openbsd_font_path = '/usr/X11R6/lib/X11/fonts'
-
-        # If MRI and/or non-Bash shell (like cmd.exe)
-        if Object.const_defined? :RUBY_PLATFORM
-          case RUBY_PLATFORM
-          when /darwin/  # macOS
-            macos_font_path
-          when /linux/
-            linux_font_path
-          when /mingw/
-            windows_font_path
-          when /openbsd/
-            openbsd_font_path
-          end
-        # If MRuby
+        system_string = if Object.const_defined? :RUBY_PLATFORM
+          # If MRI and/or non-Bash shell (like cmd.exe)
+          RUBY_PLATFORM
         else
-          uname = `uname`
-          if uname.include? 'Darwin'  # macOS
-            macos_font_path
-          elsif uname.include? 'Linux'
-            linux_font_path
-          elsif uname.include? 'MINGW'
-            windows_font_path
-          elsif uname.include? 'OpenBSD'
-            openbsd_font_path
-          end
+          # MRuby
+          `uname`
+        end
+
+        case system_string
+        when /darwin/i  # macOS
+          '/Library/Fonts'
+        when /linux/i
+          '/usr/share/fonts'
+        when /mingw/i
+          'C:/Windows/Fonts'
+        when /openbsd/i
+          '/usr/X11R6/lib/X11/fonts'
         end
       end
 
