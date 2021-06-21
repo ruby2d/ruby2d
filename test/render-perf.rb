aed8f34c15822fff67656b4812a7d80c99a7ed66
Tom Black <thomas.black@gsa.gov>
2021-06-21 17:42:54 -0500

    Update render-perf.rb

--- test/render-perf.rb
+++ test/render-perf.rb
@@ -1,6 +1,6 @@
 require 'ruby2d'
 
-set width: 1000, height: 500
+set width: 1000, height: 675
 
 fps = Text.new 'fps'
 
@@ -10,9 +10,10 @@ end
 
 render do
 
-  # 31,250 squares drawn at ~38 fps
+  # 39,500 squares drawn at ~30 fps
+  # MacBook Pro, 6-Core Intel Core i9 2.9 GHz, Radeon Pro Vega 20
   250.times do |i|
-    125.times do |j|
+    158.times do |j|
       Pixel.draw(x: i*4, y: j*4 + 26, size: 4, color: [rand, rand, rand, 1.0])
     end
   end
