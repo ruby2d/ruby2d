e573609a745ae25a5094c949f9a2e0fd15dfa17f
Tom Black <blacktm@gmail.com>
2021-07-09 19:39:31 -0500

    Implement new syntax patterns (#197)
    DSL, singleton, instance, class

--- test/window_spec.rb
+++ test/window_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Ruby2D::Window do
 
   it "returns class values" do
     expect(Window.current).to be_a(Ruby2D::Window)
-    expect(Window.width).to eq(640)
     set width: 200
     expect(Window.width).to eq(200)
   end
