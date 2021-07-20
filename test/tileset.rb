e39ac4a3293b1f2e6d5044b3ae58648be58a381f
Mario Visic <mario@mariovisic.com>
2021-07-20 17:20:03 +1000

    Update tileset test

--- test/tileset.rb
+++ test/tileset.rb
@@ -2,23 +2,35 @@ require 'ruby2d'
 
 set background: 'white'
 
-tileset = Tileset.new('test/media/texture_atlas.png', tile_width: 36, tile_height: 45, padding: 2, spacing: 0)
+
+TILE_WIDTH = 36
+TILE_HEIGHT = 45
+
+set width: 108
+set height: 135
+
+tileset = Tileset.new('test/media/texture_atlas.png', tile_width: TILE_WIDTH, tile_height: TILE_HEIGHT, padding: 0, spacing: 0)
 
 tileset.define_tile('num-1', 0, 0)
 tileset.define_tile('num-2', 1, 1)
+tileset.define_tile('num-3', 2, 0)
 
 tileset.set_tile('num-1', [
-  {x: 0,  y: 0},
-  {x: 100,  y: 100},
-  {x: 40, y: 320}
+  {x: TILE_WIDTH * 0, y: TILE_HEIGHT * 0},
+  {x: TILE_WIDTH * 1, y: TILE_HEIGHT * 1},
+  {x: TILE_WIDTH * 2, y: TILE_HEIGHT * 2}
 ])
 
 tileset.set_tile('num-2', [
-  {x: 300,  y: 0},
-  {x: 400,  y: 100},
-  {x: 500, y: 320}
+  {x: TILE_WIDTH * 0, y: TILE_HEIGHT * 1},
+  {x: TILE_WIDTH * 1, y: TILE_HEIGHT * 2},
+  {x: TILE_WIDTH * 2, y: TILE_HEIGHT * 0},
 ])
 
-# image = Image.new('test/media/texture_atlas.png', x: 10, y: 20)
+tileset.set_tile('num-3', [
+  {x: TILE_WIDTH * 0, y: TILE_HEIGHT * 2},
+  {x: TILE_WIDTH * 1, y: TILE_HEIGHT * 0},
+  {x: TILE_WIDTH * 2, y: TILE_HEIGHT * 1},
+])
 
 show
