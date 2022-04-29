fa187e2cdc7d99e60322cfac99bbfd2b4d2cce7e
nogginly <nogginly@icloud.com>
2022-04-29 17:05:40 -0400

    Add `fill_polygon` method for convex polygons with vertex coloring

--- lib/ruby2d/canvas.rb
+++ lib/ruby2d/canvas.rb
@@ -234,7 +234,6 @@ module Ruby2D
     end
 
     # Draw a poly-line between N points.
-    # @note A more general purpose method to draw N-point poly-line will eventually replace this method.
     # @param [Array] coordinates An array of numbers x1, y1, x2, y2 ... with at least three coordinates (6 values)
     # @param [Numeric] pen_width The line's thickness in pixels; defaults to 1.
     # @param [Color] color (or +colour+) The line colour
@@ -252,11 +251,39 @@ module Ruby2D
       update_texture if @update
     end
 
+    # Fill a polygon made up of N points.
+    # @note Currently only supports convex polygons or simple polygons with one concave corner.
+    # @note Supports per-vertex coloring, but the triangulation may change and affect the coloring.
+    # @param [Array] coordinates An array of numbers x1, y1, x2, y2 ... with at least three coordinates (6 values)
+    # @param [Color, Color::Set] color (or +colour+) Set one or per-vertex colour; at least one colour must be specified.
+    def fill_polygon(coordinates:, color: nil, colour: nil)
+      return if coordinates.nil? || coordinates.count < 6 || (color.nil? && colour.nil?)
+
+      ext_fill_polygon(coordinates, colors_to_a(color || colour))
+      update_texture if @update
+    end
+
     def update
       update_texture
     end
 
     private
+
+    # Converts +color_or_set+ as a sequence of colour components; e.g. +[ r1, g1, b1, a1, ...]+
+    # @param [Color, Color::Set] color_or_set
+    # @return an array of r, g, b, a values
+    def colors_to_a(color_or_set)
+      if color_or_set.is_a? Color::Set
+        color_a = []
+        color_or_set.each do |clr|
+          color_a << clr.r << clr.g << clr.b << clr.a
+        end
+        return color_a
+      end
+
+      color_or_set = Color.new(color_or_set) unless color_or_set.is_a? Color
+      color_or_set.to_a
+    end
 
     def update_texture
       @texture.delete
