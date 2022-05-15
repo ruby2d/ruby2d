b15621ccc9a75d8b5552561102d4a2f77ed712d8
nogginly <nogginly@icloud.com>
2022-05-15 12:44:20 -0400

    Tweak to load and keep using file_path if `as:` not specified

--- lib/ruby2d/pixmap_atlas.rb
+++ lib/ruby2d/pixmap_atlas.rb
@@ -6,14 +6,6 @@ module Ruby2D
   # is a dictionary of named pixmaps where each named pixmap is loaded once and can be re-used
   # multiple times without the overhead of repeated loading and multiple copies.
   class PixmapAtlas
-    ##
-    # Error when a name is already used
-    class NameInUseError < StandardError
-      def initialize(name)
-        super("Name (#{name}) in use with another pixmap")
-      end
-    end
-
     def initialize
       @atlas = {} # empty map
     end
@@ -24,29 +16,39 @@ module Ruby2D
       @atlas.clear
     end
 
+    # Find a previosly loaded Pixmap in the atlas by name
+    #
+    # @param [String] name
+    # @return [Pixmap, nil]
     def [](name)
       @atlas[name]
     end
 
+    # Get the number of Pixmaps in the atlas
     def count
       @atlas.count
     end
 
+    # Iterate through each Pixmap in the atlas
     def each(&block)
       @atlas.each(&block)
     end
 
     # Load a pixmap from an image +file+ and store it +as+ named for later lookup.
     #
-    # @throw [NameInUseError] if the name to keep the image +as+ is already in use
-    # @throw [Pixmap::InvalidImageFileError] if the image file cannot be loaded
+    # @param [String] file_path The path to the image file to load as a +Pixmap+
+    # @param [String] as The name to associated with the Pixmap; if +nil+ then +file_path+ is used +as+ the name
     #
-    # @return [Pixmap] the loaded pixmap
-    def load_and_keep_image(file_path, as:)
-      raise NameInUseError, as if @atlas.member? as
-
-      pixmap = Pixmap.new file_path
-      @atlas[as] = pixmap
+    # @raise [Pixmap::InvalidImageFileError] if the image file cannot be loaded
+    # @return [Pixmap] the new (or existing) Pixmap
+    def load_and_keep_image(file_path, as: nil)
+      name = as || file_path
+      if @atlas.member? name
+        @atlas[name]
+      else
+        pixmap = Pixmap.new file_path
+        @atlas[name] = pixmap
+      end
     end
   end
 end
