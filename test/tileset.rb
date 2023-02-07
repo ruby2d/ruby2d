6727215ed7667164205a0b084fadf46901099546
Mario Visic <mario@mariovisic.com>
2023-02-07 13:04:32 +1100

    Add the ability to get/remove and replace existing tiles
    When the tileset feature was added, the idea was that you would clear
    all of the tiles before painting new ones, this proves to be costly from
    a performance standpoint, so instead it's easier if we can sub out a
    single tile for a new one.
    
    The tilesets are kept in an array which we need to scan to remove tiles,
    I think at the moment this will likely be ok for performance, we can sub
    out the array for a hash keyed by the x+y coordinates of the tile;
    though retrieving the hash values for rendering might incur some memory
    copy cost as well, so happy to change it if we feel necessary.

--- test/tileset.rb
+++ test/tileset.rb
@@ -11,9 +11,16 @@ class GameWindow < Ruby2D::Window
     set height: 135 * SCALE
     set background: 'white'
     @tileset = Tileset.new("#{Ruby2D.test_media}/texture_atlas.png", tile_width: TILE_WIDTH, tile_height: TILE_HEIGHT, scale: SCALE)
-  end
 
-  def update
+    on :mouse_down do |action|
+      x = (action.x / SCALE / TILE_WIDTH) * TILE_WIDTH * SCALE
+      y = (action.y / SCALE / TILE_HEIGHT) * TILE_HEIGHT * SCALE
+
+      tile = @tileset.get_tile(x, y)
+      next_num = ['num-1', 'num-2', 'num-3'].cycle(2).to_a[tile.fetch(:name)[-1].to_i]
+      @tileset.replace_tiles(next_num, [{x: x, y: y}])
+    end
+
     @tileset.clear_tiles
 
     @tileset.define_tile('num-1', 0, 0, flip: :both)
@@ -37,6 +44,9 @@ class GameWindow < Ruby2D::Window
       {x: TILE_WIDTH * SCALE * 1, y: TILE_HEIGHT * SCALE * 0},
       {x: TILE_WIDTH * SCALE * 2, y: TILE_HEIGHT * SCALE * 1},
     ])
+  end
+
+  def update
   end
 
   def render
